4bacd816ed00b348165596c68635123962e79ace
Chung Wei Leong <leongchungwei@hotmail.com>
2018-03-08 14:26:51 +0800

    Added show user list function

---
+++ client/src/components/Users.vue
@@ -0,0 +1,123 @@
+<template>
+    <div class="container-fluid">
+        <div class="row">
+            <table class="table table-striped table-hover">
+                <thead>
+                    <tr>
+                        <th>ID</th>
+                        <th>Name</th>
+                        <th>Role</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr v-for="u in users" :key="u._id" data-toggle="modal" data-target="#user-modal" @click="user_Selected(u)">
+                        <td>{{ u._id }}</td>
+                        <td>{{ u.name }}</td>
+                        <td>{{ u.role==0?"Administrator":u.role==1?"Lecture":"Student" }}</td>
+                    </tr>
+                </tbody>
+            </table>
+        </div>
+
+        <!-- user detail modal -->
+        <div id="user-modal" class="modal fade">
+            <div class="modal-dialog">
+                <div class="modal-content">
+                    <div class="modal-header">
+                        <button class="close" aria-hidden="true" type="button" data-dismiss="modal">&times;</button>
+                        <h4 class="modal-title">
+                            <span class="glyphicon glyphicon-user"></span>&nbsp;&nbsp;User's Detail
+                        </h4>
+                    </div>
+                    <div class="modal-body">
+                        <img class="img-circle center-block" style="width:100px; margin-bottom:10px;" :src="selectedUser.thumnail">
+
+                        <div class="form-horizontal">
+                            <!-- id-->
+                            <div class="form-group">
+                                <label class="col-sm-3 control-label" for="userId">
+                                    <b>ID</b>
+                                </label>
+                                <div class="col-sm-9">
+                                    <p class="form-control-static" id="userId">{{ selectedUser._id }}</p>
+                                </div>
+                            </div>
+                            <!-- name-->
+                            <div class="form-group">
+                                <label class="col-sm-3 control-label" for="name">
+                                    <b>Name</b>
+                                </label>
+                                <div class="col-sm-9">
+                                    <p class="form-control-static" id="name">{{ selectedUser.name }}</p>
+                                </div>
+                            </div>
+                            <!-- email-->
+                            <div class="form-group">
+                                <label class="col-sm-3 control-label" for="email">
+                                    <b>Email</b>
+                                </label>
+                                <div class="col-sm-9">
+                                    <p class="form-control-static" id="email">{{ (!selectedUser.email || selectedUser.email.length==0) ? "-" : selectedUser.email }}</p>
+                                </div>
+                            </div>
+                            <!-- role-->
+                            <div class="form-group">
+                                <label class="col-sm-3 control-label" for="role">
+                                    <b>Role</b>
+                                </label>
+                                <div class="col-sm-9">
+                                    <p class="form-control-static" id="role">{{ selectedUser.role==0?"Administrator":selectedUser.role==1?"Lecture":"Student" }}</p>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="modal-footer">
+                        <button class="btn btn-default" type="button" data-dismiss="modal">Close</button>
+                        <button class="btn btn-danger" type="button" v-if="selectedUser._id!=user._id" data-dismiss="modal" data-toggle="modal" data-target="#delete-modal">Delete</button>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+    export default {
+      data() {
+        return {
+          users: [],
+          selectedUser: {},
+          deleteNameInput: ""
+        };
+      },
+      methods: {
+        getAllUsers() {
+          this.$http
+            .get("/users/all")
+            .then(data => {
+              return data.json();
+            })
+            .then(data => {
+              this.users = data;
+            });
+        },
+        user_Selected(user) {
+          this.selectedUser = user;
+        }
+      },
+      computed: {
+        user() {
+          return this.$store.state.user;
+        }
+      },
+      created() {
+        this.getAllUsers();
+      }
+    };
+</script>
+
+<style scoped>
+    table tbody tr {
+      cursor: pointer;
+    }
+</style>
